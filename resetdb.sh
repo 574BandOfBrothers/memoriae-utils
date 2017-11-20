7fb14bedd517766023525d6f0ee0634584db51ff
Mustafa Gonul <mustafagonul@gmail.com>
2017-11-20 23:54:37 +0300

    - Database names updated.

--- resetdb.sh
+++ resetdb.sh
@@ -11,17 +11,17 @@ mongo memoriae-dev  --eval 'db.dropDatabase()'
 mongo memoriae-test --eval 'db.dropDatabase()'
 
 # Creating Collections
-mongo memoriae      --eval 'db.createCollection("users");'
-mongo memoriae      --eval 'db.createCollection("stories");'
-mongo memoriae      --eval 'db.createCollection("annotations");'
+mongo memoriae      --eval 'db.createCollection("User");'
+mongo memoriae      --eval 'db.createCollection("Story");'
+mongo memoriae      --eval 'db.createCollection("Annotation");'
 
-mongo memoriae-dev  --eval 'db.createCollection("users");'
-mongo memoriae-dev  --eval 'db.createCollection("stories");'
-mongo memoriae-dev  --eval 'db.createCollection("annotations");'
+mongo memoriae-dev  --eval 'db.createCollection("User");'
+mongo memoriae-dev  --eval 'db.createCollection("Story");'
+mongo memoriae-dev  --eval 'db.createCollection("Annotation");'
 
-mongo memoriae-test --eval 'db.createCollection("users");'
-mongo memoriae-test --eval 'db.createCollection("stories");'
-mongo memoriae-test --eval 'db.createCollection("annotations");'
+mongo memoriae-test --eval 'db.createCollection("User");'
+mongo memoriae-test --eval 'db.createCollection("Story");'
+mongo memoriae-test --eval 'db.createCollection("Annotation");'
 
 # Creating users
 # No users should be added for production database by now.
